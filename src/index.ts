ae0c20f1ca9e3f1d35605f112f2250fbc4846c31
Anish-Rawat <anishrawat53@gmail.com>
2025-04-22 10:06:59 +0530

    setup pariksha-mitra backend

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import connectDb from "./db/index";
+import app from "./app";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port number ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Server is not running`, error);
+  });
