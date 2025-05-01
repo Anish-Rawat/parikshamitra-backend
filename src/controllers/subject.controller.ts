2722128529221289a1c387c2722d201af26de7c0
Anish-Rawat <anishrawat53@gmail.com>
2025-05-01 10:36:03 +0530

    Reslved conflict of add subject in new class with same nam eof subject in another class

--- src/controllers/subject.controller.ts
+++ src/controllers/subject.controller.ts
@@ -9,14 +9,12 @@ const getSubjects = asyncHandler(
     try {
       const response = await Subject.find();
       const totalRecords = getSubjects.length;
-      res
-        .status(201)
-        .json(
-          new ApiResponse(200, "subject fetched successfully", {
-            totalRecords,
-            result: response,
-          })
-        );
+      res.status(201).json(
+        new ApiResponse(200, "subject fetched successfully", {
+          totalRecords,
+          result: response,
+        })
+      );
     } catch (error) {
       res
         .status(500)
@@ -28,7 +26,7 @@ const getSubjects = asyncHandler(
 const addSubject = asyncHandler(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
-      const { subjectName, classId } = req.body;
+      const { subjectName, classId, totalSubjects = 0 } = req.body;
 
       if (!classId || !subjectName) {
         res.status(400).json({
@@ -38,14 +36,12 @@ const addSubject = asyncHandler(
         return;
       }
 
-      // Normalize both inputs
-      const normalizedSubject = subjectName.trim().toLowerCase();
-
       // Check if combination already exists
       const existing = await Subject.findOne({
         classId: classId,
-        subjectName: normalizedSubject,
+        subjectName: subjectName,
       });
+      console.log("existing", existing);
       if (existing) {
         res.status(409).json({
           success: false,
@@ -55,24 +51,42 @@ const addSubject = asyncHandler(
       }
 
       const isClassIdExist = await Class.findById(classId);
+      console.log("isClassIdExist", isClassIdExist);
+
       if (!isClassIdExist) {
         res
           .status(400)
           .json({ success: false, message: "No such class id exist." });
       }
       const response = await Subject.create({
-        subjectName: normalizedSubject,
-        classId,
+        subjectName: subjectName,
+        classId: classId,
+        totalSubjects: totalSubjects,
       });
 
+      // After saving subject
+      const updatedSelectedClassInfo = await Class.findByIdAndUpdate(classId, {
+        $inc: { totalSubjects: 1 },
+      });
+      console.log("updatedSelectedClassInfo", updatedSelectedClassInfo);
+      console.log("response", response);
       res
         .status(201)
         .json(new ApiResponse(200, "Subject added successfully", response));
-    } catch (error) {
+    } catch (error:unknown) {
       console.log(error);
-      res
-        .status(500)
-        .json({ success: false, message: "Internal Server Error" });
+      // Duplicate key error from MongoDB
+      if (error.code === 11000) {
+        res.status(409).json({
+          success: false,
+          message: "This subject already exists",
+        });
+      } else {
+        res.status(500).json({
+          success: false,
+          message: "Internal Server Error",
+        });
+      }
     }
   }
 );
@@ -121,8 +135,10 @@ const deleteSubject = asyncHandler(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
       const selectedSubjectId = req.params.id;
-      const isSelectedSubjectIdExist = await Subject.findById(selectedSubjectId)
-      if(!isSelectedSubjectIdExist){
+      const isSelectedSubjectIdExist = await Subject.findById(
+        selectedSubjectId
+      );
+      if (!isSelectedSubjectIdExist) {
         res
           .status(400)
           .json({ success: false, message: "No such subject id exist." });
--- src/models/class.model.ts
+++ src/models/class.model.ts
@@ -1,24 +1,28 @@
 import mongoose, { Schema } from "mongoose";
 
 const classSchema = new Schema(
-    {
-        className:{
-            type:String,
-            required:true,
-            trim:true,
-            index:true,
-        },
-        category:{
-            type:String,
-            required:true,
-            trim:true,
-            index:true,
-            enum: ["class", "stream"]
-        }
+  {
+    className: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
-    {
-        timestamps:true,
-    }
-)
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+      enum: ["class", "stream"],
+    },
+    totalSubjects: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const Class = mongoose.model('Class',classSchema);
+export const Class = mongoose.model("Class", classSchema);
