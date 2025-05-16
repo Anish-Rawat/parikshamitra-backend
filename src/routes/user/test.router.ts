e0fbc374bebbcd003647bae3c68eded106618ed2
Tamanjeet <tamanjeet1@appinventiv.com>
2025-05-16 13:02:47 +0530

    diffrentiate admin and user aois

--- src/routes/user/test.router.ts
+++ src/routes/user/test.router.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   createTest,
+  getTestInfoByUserId,
   submitTest,
 } from "../../controllers/test.controller";
 import { verifyJWT } from "../../middlewares/user/auth.middleware";
@@ -10,5 +11,8 @@ const router = Router();
 router.route("/create-test").post(verifyJWT, createTest);
 
 router.route("/submit-test").post(verifyJWT, submitTest);
+
+router.route("/get-test-by-userId/:userId").get(verifyJWT, getTestInfoByUserId);
+
 
 export default router;
